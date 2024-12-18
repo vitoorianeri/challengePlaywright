dc2b472256964b3ad3a69555cd2658f4698568ab
Vitoria Neri <vitoria.vneri@gmail.com>
2024-12-18 00:00:55 -0300

    finished the fouth question

--- pages/alertBox.ts
+++ pages/alertBox.ts
@@ -5,15 +5,21 @@ export class AlertBox {
 
     readonly page: Page
     readonly btnShowAlert: Locator
+    readonly messageShowAlert: Locator
     readonly messageConfirmation: Locator
     readonly btnConfirmBox: Locator
+    readonly btnPromptBox: Locator
+    readonly messageReturn: Locator
 
     constructor(page: Page) {
 
         this.page = page
-        this.btnShowAlert = page.locator('#alertexamples')
-        this.messageConfirmation = page.locator('#alertexplanation')
-        this.btnConfirmBox = page.locator('#confirmexample')
+        this.btnShowAlert = page.locator('#alertexamples');
+        this.messageShowAlert = page.locator('#alertexplanation');
+        this.messageConfirmation = page.locator('#confirmexplanation');
+        this.btnConfirmBox = page.locator('#confirmexample');
+        this.btnPromptBox = page.locator('#promptexample');
+        this.messageReturn = page.locator('#promptexplanation');
 
     }
 
@@ -21,35 +27,69 @@ export class AlertBox {
         await this.page.goto('https://testpages.eviltester.com/styled/alerts/alert-test.html')
     }
 
-    async clickShowAlert() {
-        await this.btnShowAlert.isVisible;
+    async configDialogPage() {
         this.page.on('dialog', async dialog => {
-            const messageDialog: string = 'I am an alert box!';
-            expect((messageDialog == dialog.message())).toEqual
-            await dialog.accept();
-            const messageTriggered: string = 'You triggered and handled the alert dialog';
-            const validateMessage: string = await this.messageConfirmation.innerText();
-            expect((messageTriggered == validateMessage)).toEqual
+
+            // Get dialog type
+            const dialogType: string = dialog.type();
+
+            if (dialogType == 'alert') { // b and c options
+
+                const messageDialog: string = 'I am an alert box!';
+                expect((messageDialog == dialog.message())).toEqual(true);
+
+                await dialog.accept();
+
+                const messageTriggered: string = 'You triggered and handled the alert dialog';
+
+                this.messageShowAlert.isVisible();
+                const validateMessage: string = await this.messageShowAlert.innerText();
+
+                expect((messageTriggered == validateMessage)).toEqual(true);
+
+            } else if (dialogType == 'confirm') { // e and f options
+
+                const messageDialog: string = 'I am a confirm alert';
+                expect((messageDialog == dialog.message())).toEqual(true);
+
+                await dialog.dismiss();
+
+                const messageTriggered: string = 'You clicked Cancel, confirm returned false.';
+
+                this.messageConfirmation.isVisible();
+                const validateMessage: string = await this.messageConfirmation.innerText();
+
+                expect((messageTriggered == validateMessage)).toEqual(true);
+
+            } else if (dialogType == 'prompt') { // h and i options
+
+                const messageDialog: string = 'I prompt you';
+                expect((messageDialog == dialog.message())).toEqual(true);
+
+                await dialog.accept('Vitória Soares');
+
+                const messageTriggered: string = "You clicked OK. 'prompt' returned Vitória Soares";
+
+                this.messageReturn.isVisible();
+                const validateMessage: string = await this.messageReturn.innerText();
+                expect((messageTriggered == validateMessage)).toEqual(true);
+
+            }
         });
+    }
+
+    async clickShowAlert() {
+        await this.btnShowAlert.isVisible();
         await this.btnShowAlert.click();
     }
 
     async clickShowConfirm() {
-        await this.btnConfirmBox.isVisible;
-        this.page.on('dialog', async dialog => {
-            const messageDialog: string = 'I am a confirm alert';
-            expect((messageDialog == dialog.message())).toEqual
-            await dialog.dismiss();
-            const messageTriggered: string = 'You clicked Cancel, confirm returned false.';
-            const validateMessage: string = await this.messageConfirmation.innerText();
-            expect((messageTriggered == validateMessage)).toEqual
-        });
+        await this.btnConfirmBox.isVisible();
         await this.btnConfirmBox.click();
-
-
     }
 
-  
-
-
+    async clickPromptBox() {
+        await this.btnPromptBox.isVisible();
+        await this.btnPromptBox.click();
+    }
 }    
--- tests/alertBox.spec.ts
+++ tests/alertBox.spec.ts
@@ -4,13 +4,10 @@ import { AlertBox } from '../pages/alertBox';
 test('4. Click in the pop-up events', async ({ page }) => {
     const alert = new AlertBox(page);
     await alert.navegatTo();
+    await alert.configDialogPage();
     await alert.clickShowAlert();
     await alert.clickShowConfirm();
-
-    
-
-
-    //expect((initialRefreshCount !== refreshedCount)).toBe(true);
+    await alert.clickPromptBox();
 
 });
 
